4d6b57bd3d6424efb9338c8132cfd567675d47f9
bijoy1234509 <bijoymondal8509121704@gmail.com>
2024-08-20 09:00:01 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,10 +8,10 @@ function convertToBool(text, fault = 'true') {
 global.thumb = fs.readFileSync('./lib/bugs/venom.jpg')
 const BOTNAME = 'VAJIRA-MD|-WA-BOT™';
 const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
-global.owner = process.env.OWNER_NUMBER  || '7548054204' ;  // ADD YOUR NUMBER WITHOUT +
+global.owner = process.env.OWNER_NUMBER  || '8509121704' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=bNVWXBDA#W5CjcTWOUn60_S_5LE2ycugxtVfNu1g5_th2Zo11leU' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=bNVWXBDA#VAJIRA-MD=TVdTCSKC#8Zc6tbMsRFe-EhrubzDCuvH2l_GFSxlELlNhkTLZDYI' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "7548054204" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
